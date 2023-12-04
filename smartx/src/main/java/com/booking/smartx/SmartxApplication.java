cf0828c977739e6f680bc73b910b23c600988bd6
JohnAdah <konetallrepo@gmail.com>
2023-12-04 08:35:56 +0100

    Initial Template for smartx

---
+++ smartx/src/main/java/com/booking/smartx/SmartxApplication.java
@@ -0,0 +1,13 @@
+package com.booking.smartx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartxApplication.class, args);
+	}
+
+}
---
+++ smartx/src/test/java/com/booking/smartx/SmartxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.booking.smartx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
